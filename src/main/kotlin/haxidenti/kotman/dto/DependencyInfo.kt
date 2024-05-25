2d5cb028d09e475aee89489990c207f2c991b06a
AldieNightStar <aldie.nightstar@gmail.com>
2024-05-25 17:05:07 +0200

    Shorter logic for reading dependencies

--- src/main/kotlin/haxidenti/kotman/dto/DependencyInfo.kt
+++ src/main/kotlin/haxidenti/kotman/dto/DependencyInfo.kt
@@ -1,13 +1,10 @@
 package haxidenti.kotman.dto
 
 data class DependencyInfo(
-    val prefix: String,
     val dependency: String,
-    val dependencySuffix: String
+    val testing: Boolean
 ) {
     override fun toString(): String {
         return if (testing) "TEST: $dependency" else "IMPL: $dependency"
     }
-
-    val testing: Boolean get() = prefix.lowercase().contains("test")
 }
--- src/main/kotlin/haxidenti/kotman/util/DependencyUtil.kt
+++ src/main/kotlin/haxidenti/kotman/util/DependencyUtil.kt
@@ -3,28 +3,11 @@ package haxidenti.kotman.util
 import haxidenti.kotman.dto.DependencyInfo
 
 object DependencyUtil {
-    private val LIST = listOf(
-        "implementation(kotlin(",
-        "testCompileClasspath(",
-        "testImplementation(",
-        "compileClasspath(",
-        "testRuntimeOnly(",
-        "testCompileOnly(",
-        "implementation(",
-        "compile(",
-    )
 
     fun readDependencyString(line: String): DependencyInfo? {
-        for (prefix in LIST) {
-            val result = readStartingWith(line, prefix)
-            if (result != null) {
-                val depString = result.substringAfter("\"").substringBefore("\"")
-                val brackets = prefix.count { it == '(' }
-                val suffix = ")".repeat(brackets)
-                return DependencyInfo(prefix, depString, suffix)
-            }
-        }
-        return null
+        if (!line.contains("(") || !line.contains(")")) return null
+        val depString = line.substringAfter("\"").substringBefore("\"")
+        return DependencyInfo(depString, line.substringBefore("\"").lowercase().contains("test"))
     }
 
     private fun readStartingWith(line: String, prefix: String): String? =
