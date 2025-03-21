c280234605115f252fc6c6c338ded66ef15cf38b
AldieNightStar <AldieNightStar@gmail.com>
2025-03-21 19:53:55 +0100

    Update Kotlin version to 2.1.20 in UserConfiguration

--- src/main/kotlin/haxidenti/kotman/dto/UserConfiguration.kt
+++ src/main/kotlin/haxidenti/kotman/dto/UserConfiguration.kt
@@ -2,7 +2,7 @@ package haxidenti.kotman.dto
 
 data class UserConfiguration(
     var author: String = "HaxiDenti",
-    var kotlinVer: String = "2.0.0",
+    var kotlinVer: String = "2.1.20",
     var projectVersion: String = "1.0.0",
     var coroutineVer: String = "1.9.0-RC"
 )
